0cc7b6b28d15c8d7dec7f2a935481a0c2c651c16
ashutosh ambekar <ashuambekar@gmail.com>
2023-08-31 10:06:13 +0000

    initial commit

---
+++ app/risks/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.ns.risks.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
